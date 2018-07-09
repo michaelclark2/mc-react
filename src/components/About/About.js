e77fa02aefd0b454d5e5d5538831f1e555dea6b8
michael <michael.clark1992@gmail.com>
2018-07-09 10:40:58 -0500

    added routers

---
+++ src/components/About/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './About.css';
+
+class About extends React.Component {
+  render () {
+    return (
+      <div className="About">
+        <h1>Michael Clark</h1>
+        <p>Wubba lubba dub dub</p>
+        <p>Wubba lubba dub dub</p>
+      </div>
+    );
+  }
+}
+export default About;
