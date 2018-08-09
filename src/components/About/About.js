bc1ac9914836b9eadd84266f58e6c260b7815296
michael <michael.clark1992@gmail.com>
2018-08-08 20:56:08 -0500

    added heros, started technologies component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,9 +5,30 @@ class About extends React.Component {
   render () {
     return (
       <div className="About">
-        <h1>Michael Clark</h1>
-        <p>Wubba lubba dub dub</p>
-        <p>Wubba lubba dub dub</p>
+        <div className="hero is-fullheight intro">
+          <div className="hero-body has-text-centered">
+            <div className="container">
+              <h1 className="title">Hi,</h1>
+              <h2 className="subtitle">Let's build things.</h2>
+            </div>
+          </div>
+        </div>
+        <div className="hero is-fullheight">
+          <div className="hero-body">
+            <div className="container">
+              <div className="columns">
+                <div className="column">
+                  <h1 className="title">Technologies</h1>
+                  <h2 className="subtitle">These are the skills and tools I have aquired along my journey</h2>
+                </div>
+                <div className="column">
+                  {/* <Technologies /> */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
     );
   }
