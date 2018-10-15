dcafc76fe2bbb1e892d3170d58cfa490acd9b97a
michael <michael.clark1992@gmail.com>
2018-10-14 23:35:26 -0500

    fixed header

--- fixdates.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-const moment = require('moment');
-axios.get('https://michaelclark-112e5.firebaseio.com/blogs.json').then(res => {
-  Object.entries(res.data).forEach(entry => {
-    entry[1].date = (moment(entry[1].date));
-    axios.put('https://michaelclark-112e5.firebaseio.com/blogs/' + entry[0] + '.json', entry[1]);
-  });
-});
