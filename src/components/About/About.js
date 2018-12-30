ed473e5c0606739efb18867ec4f91b40d1247be4
michael <michael.clark1992@gmail.com>
2018-12-30 01:53:05 -0600

    touched up styling, started better landing page, found good color theme

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,19 +7,29 @@ class About extends React.Component {
     return (
       <div className="About">
         <div className="about-me section">
-          <div className="columns">
-            <div className="column">
+          <div className="columns is-multiline">
+            <div className="column is-full">
+              <div className="intro has-text-centered">
+                <div className="hero is-medium">
+                  <div className="hero-body">
+                    <div className="container is-fluid">
+                      <h3 className="title">Hi</h3>
+                      <h5 className="subtitle">Let's build things!</h5>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="column is-full">
               <figure className="image profile">
                 <img className="is-rounded" src="https://picsum.photos/500/500/?random" alt="profile pic"/>
               </figure>
             </div>
-            <div className="column">
-              <div className="intro has-text-centered">
-                <h3 className="title">Hi</h3>
-                <h5 className="subtitle">Let's build things!</h5>
-              </div>
+            <div className="column is-full">
+              <h3 className="title is-1">As an artist,</h3>
               <p>I enjoy creating unique and enjoyable experiences. A graphic artist turned developer, software development was the only thing that seemed to satisfy my constant desire to learn and experiment.</p>
             </div>
+
           </div>
         </div>
         <Technologies />
